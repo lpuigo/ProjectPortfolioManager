4b40c00cd303bd3f1117bff22994774cd7f4ea75
lpuigo <laurent.puig@gmail.com>
2018-04-14 20:52:50 +0200

    WL progress bar comp Done

--- src/client/hvue/comps/project_table/wl_progress_bar/template.go
+++ src/client/hvue/comps/project_table/wl_progress_bar/template.go
@@ -1,9 +1,16 @@
 package wl_progress_bar
 
 const template = `
-<el-progress 
-	:show-text="false"
-	:stroke-width="10" 
-	:percentage="progressPct"
-></el-progress>
+<div>
+    <div v-if="showProgressBar" :class="progressStatus">
+        <el-progress 
+                 :show-text="false"
+                 :stroke-width="5"
+                 :percentage="progressPct"
+        ></el-progress>
+        <span class="small-font">{{ project | chargeFormat }}</span>
+    </div>
+    <span v-else>{{ project | chargeFormat }}</span>
+</div>
+
 `
--- src/client/hvue/comps/project_table/wl_progress_bar/wl_progress_bar_comp.go
+++ src/client/hvue/comps/project_table/wl_progress_bar/wl_progress_bar_comp.go
@@ -5,6 +5,7 @@ import (
 	"github.com/huckridgesw/hvue"
 	fm "github.com/lpuig/novagile/src/client/frontmodel"
 	"github.com/lpuig/novagile/src/client/hvue/tools"
+	"strconv"
 )
 
 func Register() {
@@ -22,8 +23,19 @@ func ComponentOptions() []hvue.ComponentOption {
 		}),
 		hvue.MethodsOf(&ProjectProgressBarModel{}),
 		hvue.Computed("progressPct", func(vm *hvue.VM) interface{} {
-			ppbm := &ProjectProgressBarModel{Object:vm.Object}
+			ppbm := &ProjectProgressBarModel{Object: vm.Object}
 			return ppbm.ProgressPct()
+		}),
+		hvue.Computed("showProgressBar", func(vm *hvue.VM) interface{} {
+			ppbm := &ProjectProgressBarModel{Object: vm.Object}
+			return ppbm.Project.ForecastWL > 0 // && ppbm.Project.CurrentWL > 0
+		}),
+		hvue.Computed("progressStatus", func(vm *hvue.VM) interface{} {
+			ppbm := &ProjectProgressBarModel{Object: vm.Object}
+			return ppbm.ProgressStatus()
+		}),
+		hvue.Filter("chargeFormat", func(vm *hvue.VM, prj *js.Object, args ...*js.Object) interface{} {
+			return ProjectChargeFormat(fm.ProjectFromJS(prj))
 		}),
 	}
 }
@@ -45,17 +57,43 @@ func NewProjectProgressBarModel(vm *hvue.VM) *ProjectProgressBarModel {
 	return ptm
 }
 
-func (p *ProjectProgressBarModel) ProgressPct() (pct float64) {
-	pp := p.Project
+// Computed related Methods
+//
+
+func (pb *ProjectProgressBarModel) ProgressPct() (pct float64) {
+	pp := pb.Project
 	if pp.ForecastWL > 0 {
-		pct = pp.CurrentWL/pp.ForecastWL*100
+		pct = pp.CurrentWL / pp.ForecastWL * 100
 	} else {
-		// TODO Manage unknown forcast
 		pct = 50
 	}
 	if pct > 100 {
-		// TODO Manage WL Overspent
 		pct = 100
 	}
+	return
+}
+func (pb *ProjectProgressBarModel) ProgressStatus() (res string) {
+	pp := pb.Project
+	if pp.ForecastWL > 0 && pp.CurrentWL > pp.ForecastWL {
+		res = "over-spent"
+		return
+	}
+	//if pp.Status == "6 - Done" {
+	//	res = "sucess"
+	//}
+	return
+}
+
+// Filter related Funcs
+//
+
+func ProjectChargeFormat(p *fm.Project) (res string) {
+	if p.ForecastWL > 0 || p.CurrentWL > 0 {
+		res = strconv.FormatFloat(p.CurrentWL, 'f', 1, 64)
+		res += " / "
+		res += strconv.FormatFloat(p.ForecastWL, 'f', 1, 64)
+		return
+	}
+	res = "-"
 	return
 }
