2711d33870719573eb7df52817377b7c9c559312
lpuigo <laurent.puig@gmail.com>
2018-05-04 16:33:28 +0200

    FE timeout augmented

--- src/client/tools/const.go
+++ src/client/tools/const.go
@@ -3,8 +3,8 @@ package tools
 const (
 	SuccessMsgDuration = 2000
 	WarningMsgDuration = 5000
-	LongTimeOut        = 2000
-	TimeOut            = 1000
+	LongTimeOut        = 5000
+	TimeOut            = 2000
 )
 
 
